444e860a84bcb14be2712b680f87c153a0178f08
Harendra <2199420@cognizant.com>
2024-06-19 17:55:27 +0530

    Second Day

--- src/main/java/com/spring/controller/HomeController.java
+++ src/main/java/com/spring/controller/HomeController.java
@@ -1,17 +1,104 @@
 package com.spring.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.entities.Employee;
+
 @RestController
+@RequestMapping("/api/")
 public class HomeController {
-	
+	List<Employee> al = new ArrayList<>();// instance variable . we can access value as global
 	
 	@GetMapping({"/","/index","/home"})
 	public String defaultUrl()
 	{
 		return "Home Page";
 	}
+	
+		
+	// if you want save data then we can use @postMapping
+	// we have created rest api as a post api to create new resource.
+	@PostMapping("/save")
+	public String saveData( @RequestBody Employee emp)
+	{// @requestBody Annotation is used to convert json data in to object
+		
+		al.add(emp);
+		
+		return "data has been successfully...";// 
+	}
+	
+	// if you want to fetch data then we can use @Getmapping
+	// we have created rest api to get data from server 
+	@GetMapping("/getdata")
+	public List<Employee> getData()
+	{
+		System.out.println("calling getAPI...."+al);// agr 
+	
+		/*for (Employee employee : al) {
+			
+			System.out.println(employee.getEmail());
+			System.out.println(employee.getEmpid());
+			System.out.println(employee.getFname());
+			System.out.println(employee.getLname());
+			System.out.println(employee.getMobile());
+			
+		}
+		*/
+		return al;
+		
+	}
+	
+	// if you want to delete data we can use @DeleteMapping 
+	// we have created rest api to delete from arraylist
+	@DeleteMapping("/delete")
+	public String deleteData()
+	{
+		System.out.println("Calling Delete API...");
+		
+		al.remove(0);
+		return "data has been deleted successfully...";
+		
+		
+	}
+	
+	// if you wnt to update exits data then we can use @PutMapping 
+	//we have created rest api to update data from arraylist
+	@PutMapping("/update")
+	public String updateData( @RequestBody Employee emp)
+	{
+		System.out.println(" Calling Update API...");
+		
+		al.set(0, emp);
+		
+		return "data udpated successfully...";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	@GetMapping("/hey")
 	public String heyKomal()
@@ -20,9 +107,13 @@ public class HomeController {
 	}
 	
 	@GetMapping("/hi")
+	//@RequestMapping(value="/hi",method=RequestMethod.GET)
+	
 	public String heyHarendra()
 	{
 		return "Hi Harendra, Welcome to Springboot Application";
 	}
+	
+	
 
 }
---
+++ src/main/java/com/spring/entities/Employee.java
@@ -0,0 +1,44 @@
+package com.spring.entities;
+
+
+
+public class Employee {
+	
+	private int empid;
+	private String fname;
+	private String lname;
+	private String mobile;
+	private String email;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
