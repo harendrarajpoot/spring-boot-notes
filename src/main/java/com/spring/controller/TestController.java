60cc42709565b5590a41d4a64f6e505af9bdb6ee
Harendra <2199420@cognizant.com>
2024-06-21 17:59:59 +0530

    test file created and updated iin to testcontroller

--- src/main/java/com/spring/controller/TestController.java
+++ src/main/java/com/spring/controller/TestController.java
@@ -1,5 +1,17 @@
 package com.spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+	
+	
+	@GetMapping("/test")
+	public String testPage()
+	{
+		return "test";// return as a html page
+	}
+	
 
 }
